3be9d2e055acbbc031d1ccb0cfdc6ad4170944c4
shivam <shivam17101@gmail.com>
2022-07-08 13:05:40 +0530

    Dashboard screen

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from "react-dom"
 import Login from "./Components/UserLogin/Login"
 import SignUp from "./Components/UserSignup/Signup"
+import Dashboard from "./Components/Dashboard/index"
 
-
-ReactDOM.render(<SignUp />, document.getElementById("root"));
+ReactDOM.render(<Dashboard />, document.getElementById("root"));
 
